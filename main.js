26ae641ad89c7710e2ab8fc34043ff1549e16597
Lifeislife1992 <life.is.life1992@gmail.com>
2015-06-17 18:01:18 +0300

    documentation added

--- main.js
+++ main.js
@@ -5,7 +5,38 @@ function notEmpty(inputField, helpText){
 
         //Проверка наличия span по id и добавляем в него вспомагательный текст
         if(helpText != null){
-            helpText.innerHTML = "This field can`t be empty!";
+
+            var textId = helpText;
+            function addText() {
+                textId.innerHTML = "This field can`t be empty!";
+            }
+
+            setTimeout(addText, 200);
+            inputField.style.marginBottom = "10px";
+        }
+        return false;
+    }
+    else{
+        //Если данные обнаружены убедимся в наличии span и обнулим его значение
+        if(helpText != null){
+            helpText.innerHTML = "";
+            inputField.style.marginBottom = "0";
+        }
+        return true;
+    }
+}
+
+function validField(minSize, maxSize, inputField, helpText){
+    if(inputField.value.length < minSize || inputField.value.length > maxSize){
+        if(helpText != null){
+
+            var textId = helpText;
+            function addText() {
+                textId.innerHTML = "This field must be more then 5 or less 32 symbols!";
+            }
+
+            setTimeout(addText, 200);
+            inputField.style.marginBottom = "10px";
         }
         return false;
     }
@@ -18,18 +49,51 @@ function notEmpty(inputField, helpText){
     }
 }
 
-function validField(minSize, maxSize, inputField, helpText){
-    if(inputField.value.length < minSize || inputField.value.length > maxSize){
+function validZipField(inputField, helpText){
+    if(inputField.value.length != 5){
         if(helpText != null){
-            helpText.innerHTML = "This field must be more then 5 or less 32 symbols!";
+
+            var textId = helpText;
+            function addText() {
+                textId.innerHTML = "Zip code must contain 5 symbols";
+            }
+
+            setTimeout(addText, 200);
+            inputField.style.marginBottom = "10px";
+        }
+        return false;
+    }
+    else if(isNaN(inputField.value)){
+        if(helpText != null){
+
+            var textId = helpText;
+            function addText() {
+                textId.innerHTML = "Zip code must contain only numbers";
+            }
+
+            setTimeout(addText, 200);
+            inputField.style.marginBottom = "10px";
         }
         return false;
     }
     else{
-        //Если данные обнаружены убедимся в наличии span и обнулим его значение
         if(helpText != null){
             helpText.innerHTML = "";
         }
         return true;
+    }
+}
+
+function placeOrder(form){
+    if (validZipField(form["zip"], form["zip_help"]) &&
+        validField(form["massage"], form["massage_help"]) &&
+        notEmpty(form["date"], form["date_help"]) &&
+        notEmpty(form["name"], form["name_help"]) &&
+        notEmpty(form["phone"], form["phone_help"]) &&
+        notEmpty(form["address"], form["address_help"])){
+        form.submit();
+    }
+    else{
+        alert("Form didn`t filled correct, please check out some mistakes!");
     }
 }
